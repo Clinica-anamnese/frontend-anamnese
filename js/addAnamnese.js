77545a2d1812650445ece05bad36f8a6f153700c
João Pedro Máximo <joao.maximo@fatec.com>
2024-05-17 19:08:51 -0300

    anamnese pacienteNome, listar anamneses, tabs fix

--- js/addAnamnese.js
+++ js/addAnamnese.js
@@ -22,8 +22,8 @@ const fConsistenciaEvacuacao = document.getElementById("consistenciaEvacuacao");
 const fPraticaAtvFisica = document.getElementById("praticaAtvFisica");
 const fAtvFisica = document.getElementById("atvFisica");
 
-var currentTab = 0; // Current tab is set to be the first tab (0)
-showTab(currentTab); // Display the current tab
+var currentTab = 0;
+showTab(currentTab);
 
 // exibe os Anamneses no select do formulario
 function listarPacientesSelect() {
@@ -48,71 +48,61 @@ function listarPacientesSelect() {
 }
 
 function cadastrarAnamnese() {
-  console.log("Karai jao");
-  fetch(urlApi + endpointAnamneses, {
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization": `${token}`
-    },
-    method: "POST",
-    body: JSON.stringify({
-      pacienteId: fPacienteId.value,
-      escolaridade: fEscolaridade.value,
-      periodoEstudo: fPeriodoEstudo.value,
-      lancheEstudo: fLancheEstudo.checked,
-      periodoTrabalho: fPeriodoTrabalho.value,
-      lancheTrabalho: fLancheTrabalho.checked,
-      profissao: fProfissao.value,
-      rendaFamiliar: fRendaFamiliar.value,
-      numPessoasDomicilio: fNumPessoasDomicilio.value,
-      motivo: fMotivo.value,
-      apresentaDoenca: fApresentaDoenca.value,
-      antecedentesFamiliares: fAntecedentesFamiliares.value,
-      medicamentosContinuos: fMedicamentosContinuos.value,
-      suplementosComplementos: fSuplementosComplementos.value,
-      frequenciaEvacuacao: fFrequenciaEvacuacao.value,
-      consistenciaEvacuacao: fConsistenciaEvacuacao.value,
-      praticaAtvFisica: fPraticaAtvFisica.checked,
-      atvFisica: fAtvFisica.value
-    })
+  return new Promise((resolve, reject) => {
+    if (validateForm(formAddAnamnese)) {
+      fetch(urlApi + endpointAnamneses, {
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `${token}`
+        },
+        method: "POST",
+        body: JSON.stringify({
+          pacienteId: fPacienteId.value,
+          escolaridade: fEscolaridade.value,
+          periodoEstudo: fPeriodoEstudo.value,
+          lancheEstudo: fLancheEstudo.checked,
+          periodoTrabalho: fPeriodoTrabalho.value,
+          lancheTrabalho: fLancheTrabalho.checked,
+          profissao: fProfissao.value,
+          rendaFamiliar: fRendaFamiliar.value,
+          numPessoasDomicilio: fNumPessoasDomicilio.value,
+          motivo: fMotivo.value,
+          apresentaDoenca: fApresentaDoenca.value,
+          antecedentesFamiliares: fAntecedentesFamiliares.value,
+          medicamentosContinuos: fMedicamentosContinuos.value,
+          suplementosComplementos: fSuplementosComplementos.value,
+          frequenciaEvacuacao: fFrequenciaEvacuacao.value,
+          consistenciaEvacuacao: fConsistenciaEvacuacao.value,
+          praticaAtvFisica: fPraticaAtvFisica.checked,
+          atvFisica: fAtvFisica.value
+        })
+      })
+        .then(response => {
+          if (!response.ok) {
+            forbidden = true;
+            return Promise.reject();
+          }
+          goodWarning.textContent = "Anamnese cadastrada com sucesso!";
+          resetForm(formAddAnamnese);
+          resolve(response);
+        })
+        .catch(error => {
+          if (forbidden) {
+            badWarning.textContent = "Dados inválidos.";
+          } else {
+            badWarning.textContent = "Erro na comunicação com a API.";
+          }
+          reject(error);
+        })
+    }
   })
-    .then(response => {
-      console.log("awdawda");
-      if (!response.ok) {
-        console.log("22222222222");
-        forbidden = true;
-        // return Promise.reject();
-      }
-      goodWarning.textContent = "Anamnese cadastrada com sucesso!";
-      resetForm(formAddAnamnese);
-      // resolve(response);
-    })
-    .catch(error => {
-      console.log(error);
-      console.error(error);
-      console.log("awdawda");
-      if (forbidden) {
-        console.log(error);
-        console.log("awdawda");
-        badWarning.textContent = "Dados inválidos.";
-      } else {
-        console.log(error);
-        console.log("awdawda");
-        badWarning.textContent = "Erro na comunicação com a API.";
-      }
-      // reject(error);
-    });
-  console.log("depoisdofetch");
 }
 
 function showTab(n) {
-  console.log(n);
-  // This function will display the specified tab of the form ...
   var x = document.getElementsByClassName("tab");
   x[n].style.display = "flex";
   x[n].style.flexDirection = "column";
   x[n].style.gap = "10px";
-  // ... and fix the Previous/Next buttons:
   if (n == 0) {
     prevBtn.style.display = "none";
     divStepButtons.style.flexDirection = "row-reverse"
@@ -125,48 +115,34 @@ function showTab(n) {
   } else {
     nextBtn.textContent = "Próximo";
   }
-  // ... and run a function that displays the correct step indicator:
   fixStepIndicator(n)
 }
 
-function nextPrev(n, event) {
+async function nextPrev(n, event) {
   event.preventDefault();
-  // This function will figure out which tab to display
   var x = document.getElementsByClassName("tab");
-  // Hide the current tab:
-  x[currentTab].style.display = "none";
-  // Increase or decrease the current tab by 1:
-  currentTab = currentTab + n;
-  // if you have reached the end of the form... :
-  if (currentTab >= x.length) {
-    cadastrarAnamnese();
+  if (currentTab >= x.length - 1) {
+    try {
+      await cadastrarAnamnese();
+      window.location.href = "formularios.html";
+    }
+    catch (error) {
+      verificarAutenticacao();
+    }
   } else {
+    x[currentTab].style.display = "none";
+    currentTab += n;
     showTab(currentTab);
   }
-  // Otherwise, display the correct tab:
 }
 
 function fixStepIndicator(n) {
-  // This function removes the "active" class of all steps...
   var i, x = document.getElementsByClassName("step");
   for (i = 0; i < x.length; i++) {
     x[i].className = x[i].className.replace(" active", "");
   }
-  //... and adds the "active" class to the current step:
   x[n].className += " active";
 }
 
-/* formAddAnamnese.addEventListener("submit", async event => {
-  event.preventDefault();
-  badWarning.textContent = "";
-  goodWarning.textContent = "";
-  try {
-    await cadastrarAnamnese();
-    window.location.href = "formularios.html";
-  } catch {
-    console.log("hohow");
-    verificarAutenticacao();
-  }
-}); */
-
+verificarAutenticacao();
 listarPacientesSelect();
---
+++ js/formularios.js
@@ -0,0 +1,61 @@
+const formAddAnamnese = document.querySelector(".formAddAnamnese");
+const fNome = document.getElementById("nome");
+const fSexo = document.getElementById("sexo");
+const fDataNasc = document.getElementById("dataNasc");
+let itensTabela = "";
+
+function listarAnamneses() {
+    fetch(urlApi + endpointAnamneses, {
+        headers: {
+            "Authorization": `${token}`
+        }
+    })
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(anamnese => {
+                const itemTabela = document.createElement("tr");
+                itemTabela.classList.add("itemTabela");
+                itemTabela.id = anamnese.id;
+                tbody.appendChild(itemTabela);
+                const colunaId = document.createElement("th");
+                colunaId.textContent = `${anamnese.id}`
+                itemTabela.appendChild(colunaId);
+                const colunaPaciente = document.createElement("td");
+                colunaPaciente.textContent = `${anamnese.pacienteNome}`
+                itemTabela.appendChild(colunaPaciente);
+                const colunaAt = document.createElement("td");
+                const dataValue = anamnese.criadoEm;
+                const partes = dataValue.split("-");
+                const dataFormatada = partes[2] + "/" + partes[1] + "/" + partes[0];
+                colunaAt.textContent = dataFormatada;
+                itemTabela.appendChild(colunaAt);
+            });
+            let itensTabela = document.querySelectorAll(".itemTabela");
+            itensTabela.forEach((anamnese) => {
+                anamnese.addEventListener("click", () => {
+                    localStorage.setItem("anamneseId", anamnese.id);
+                    window.location.href = "anamnese.html";
+                })
+            })
+        })
+        .catch(error => {
+            console.error(error);
+            fallback.textContent = "Sem conexão com a API.";
+        })
+}
+
+/* formAddAnamnese.addEventListener("submit", async event => {
+    event.preventDefault();
+    badWarning.textContent = "";
+    goodWarning.textContent = "";
+    try {
+        await cadastrarAnamnese();
+        limparTabela();
+        listarAnamneses();
+    } catch {
+        verificarAutenticacao();
+    }
+}); */
+
+verificarAutenticacao();
+listarAnamneses();
